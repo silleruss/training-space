5f729757e4eca3ed2434bb53a9a9bfe9233f175d
silleruss <silleruss@gmail.com>
2021-09-26 18:41:34 +0900

    Fix health controller

--- services/central/src/main/kotlin/com/silleruss/central/controller/health/HealthController.kt
+++ services/central/src/main/kotlin/com/silleruss/central/controller/health/HealthController.kt
@@ -1,15 +1,14 @@
 package com.silleruss.central.controller.health
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import reactor.core.publisher.Mono
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/health")
 class HealthController {
 
     @GetMapping
-    fun health(): Mono<String> = Mono.just("on")
+    fun health() = "On"
 
 }
