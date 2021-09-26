cf514cbccad51d5628ea1b88caf7d43fab890997
silleruss <silleruss@gmail.com>
2021-09-26 18:41:20 +0900

    Add config webclient

---
+++ services/central/src/main/kotlin/com/silleruss/central/config/WebClientConfiguration.kt
@@ -0,0 +1,21 @@
+package com.silleruss.central.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientConfiguration(
+    @Value("\${cicada.endpoint.temp-api}") private val mainUrl: String,
+    private val webClientBuilder: WebClient.Builder
+) {
+
+    @Bean("TempApi")
+    fun webClientApi(): WebClient {
+        return webClientBuilder
+            .baseUrl(mainUrl)
+            .build()
+    }
+
+}
