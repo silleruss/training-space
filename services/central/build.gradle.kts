b4391a5d44b8ac24ab4b0aa6634e8900aeabb29e
srlee <silleruss@gmail.com>
2022-02-22 23:34:56 +0900

    feat: h2, jpa dependencies

--- services/central/build.gradle.kts
+++ services/central/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux:2.6.3")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.3")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -25,6 +26,7 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools:2.6.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.3")
 	testImplementation("io.projectreactor:reactor-test:3.4.14")
+	testImplementation("com.h2database:h2:2.1.210")
 }
 
 tasks.withType<KotlinCompile> {
