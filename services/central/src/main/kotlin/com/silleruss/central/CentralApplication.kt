df89cb94999079e966bf3daf88fbaee558f2df10
srlee <silleruss@gmail.com>
2022-02-23 23:10:32 +0900

    fix: package scan

--- services/central/src/main/kotlin/com/silleruss/central/CentralApplication.kt
+++ services/central/src/main/kotlin/com/silleruss/central/CentralApplication.kt
@@ -3,8 +3,10 @@ package com.silleruss.central
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootApplication(scanBasePackages = ["com.silleruss.central"])
 class CentralApplication
 
 fun main(args: Array<String>) {
