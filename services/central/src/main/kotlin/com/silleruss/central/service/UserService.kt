2e2b0dbcb0a6e566a18b100c27ff545b659aa7c4
srlee <silleruss@gmail.com>
2022-02-26 22:53:46 +0900

    feat: batch insert function

--- services/central/src/main/kotlin/com/silleruss/central/service/UserService.kt
+++ services/central/src/main/kotlin/com/silleruss/central/service/UserService.kt
@@ -8,15 +8,19 @@ import com.silleruss.central.model.users.CreateUserRequest
 import com.silleruss.central.model.users.UpdateUserRequest
 import com.silleruss.central.module.User
 import com.silleruss.central.repository.UserRepository
+import org.hibernate.Session
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 import javax.transaction.Transactional
 
 @Service
 @Transactional
 class UserService(
-    private val repository: UserRepository
+    private val repository: UserRepository,
+    @PersistenceContext private val entityManager: EntityManager
 ) {
 
     fun get(id: Int): Mono<UserDto> {
@@ -60,5 +64,39 @@ class UserService(
         nickname = nickname,
         email = email
     )
+
+    private fun unwrapSession() = entityManager.unwrap(Session::class.java)
+
+    // batch insert dummy users
+    private fun batchInsert(userIds: List<Int>): Unit {
+        val values = userIds.map { "${it}, $randomString, ${""}" }
+
+        val sql = """
+            insert ignore into users (
+                id, nickname, email
+            ) values ${values.joinToString { "," }}
+        """.trimIndent()
+
+        unwrapSession()
+            .createNativeQuery(sql)
+            .executeUpdate()
+    }
+
+    private fun batchDelete(): Either<Throwable, Long> {
+        TODO("Batch delete with hibernate pageable")
+    }
+
+    private val randomString = (1..RANDOM_NAME_LENGTH)
+        .map { kotlin.random.Random.nextInt(0, charPool.size) }
+        .map(charPool::get)
+        .joinToString("");
+
+    companion object {
+
+        private val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        private const val DEFAULT_INSERT_BATCH_SIZE = 100
+        private const val RANDOM_NAME_LENGTH = 10
+
+    }
 
 }
